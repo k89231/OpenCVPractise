850b4898594c5e716ca16b7d30a4b891dbab1885
Torres <yu-zheng15@mails.tsinghua.edu.cn>
2017-10-05 10:37:00 +0800

    Add trackbar to the ROI mixing section

--- ROIMix/ROIMix/MainROIMix.cpp
+++ ROIMix/ROIMix/MainROIMix.cpp
@@ -4,11 +4,16 @@
 #include<opencv2\imgproc.hpp>
 using namespace std;
 using namespace cv;
-
+const int MaxValue = 100;
+int CurrentValue = 30;
+double WeightSrc1;
+double WeighrSrc2;
+void On_Trackbar(int, void*);
+Mat src1ROI, src2ROI, src1, src2;
 int main()
 {
-	Mat src1 = imread("src1.jpg");
-	Mat src2 = imread("src2.jpg");
+	src1 = imread("src1.jpg");
+	src2 = imread("src2.jpg");
 	if ((!src1.data) || (!src2.data))
 	{
 		cout << "Fail to load the images!" << endl;
@@ -18,12 +23,24 @@ int main()
 	int y = 60;
 	int width = 100;
 	int height = 100;
-	double alpha = 0.5; double beta = 1 - alpha;
-	Mat src1ROI = src1(Rect(x, y, width, height));
-	Mat src2ROI = src2(Rect(178, 50, width, height));
-	addWeighted(src1ROI, 0.3, src2ROI, 0.7, 0, src2ROI);
-	imshow("After alpha mixing", src2);
+	src1ROI = src1(Rect(x, y, width, height));
+	src2ROI = src2(Rect(178, 50, width, height));
+	namedWindow("After Mixing", WINDOW_NORMAL);
+	createTrackbar("Adjusting weights", "After Mixing", &CurrentValue, MaxValue, On_Trackbar);
+
+	On_Trackbar(CurrentValue, 0);
+
+
+	
 	waitKey(0);
 
 	return 0;
+}
+
+void On_Trackbar(int, void*)
+{
+	WeightSrc1 = (double)getTrackbarPos("Adjusting weights", "After Mixing") / MaxValue;
+	WeighrSrc2 = 1.0 - WeightSrc1;
+	addWeighted(src1ROI, WeightSrc1, src2ROI, WeighrSrc2, 0, src2ROI);
+	imshow("After Mixing", src2);
 }
